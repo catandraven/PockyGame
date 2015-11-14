f296c7dbe66005b133e97884de290675be631e60
catandraven <amigurumibear@gmail.com>
2015-11-14 20:24:52 +0900

    Pockygame_ver1.1

--- common.js
+++ common.js
@@ -5,6 +5,8 @@ function Point(){
 
 function Character(){
 	this.position = new Point();
+	this.speed = 8;
+	this.mode = 0;
 }
 
 function Shot(){
@@ -13,8 +15,34 @@ function Shot(){
 	this.alive = false;
 }
 
+Character.prototype.move = function(dir){
+	switch(dir){
+		case "up":
+			if(this.position.y > 0){
+				this.position.y -= this.speed;
+			}
+			break;
+		case "down":
+			if(this.position.y < 360 - 48){
+				this.position.y += this.speed;
+			}
+			break;
+		case "right":
+			if(this.position.x < 360 - 32){
+				this.position.x += this.speed;
+			}
+			break;
+		case "left":
+			if(this.position.x > 0){
+				this.position.x -= this.speed;
+			}
+			break;
+	}
+}
+
+
 Shot.prototype.set = function(p,speed){
-	this.position.x = p.x + 45;
+	this.position.x = p.x + 15;
 	this.position.y = p.y;
 	this.speed = speed;
 	this.alive = true;
@@ -33,6 +61,7 @@ function Enemy(){
 	this.speed = 0;
 	this.type = null;
 	this.alive = false;
+	this.count = 0;
 }
 
 Enemy.prototype.set = function(p,type,speed){
@@ -41,9 +70,11 @@ Enemy.prototype.set = function(p,type,speed){
 	this.speed = speed;
 	this.type = type;
 	this.alive = true;
+	this.count = 0;
 }
 
 Enemy.prototype.move = function(){
+	this.count++;
 	switch(this.type){
 		case 0:
 			this.position.x += this.speed;
@@ -57,15 +88,49 @@ Enemy.prototype.move = function(){
 	}
 }
 
-function Bomb(){
+function EnemyShot(){
+	this.position = new Point();
+	this.vector = new Point();
+	this.speed = 0;
+	this.count = 0;
+	this.alive = false;
+}
+
+EnemyShot.prototype.set = function(enemyPos,myPos){
+	this.position.x = enemyPos.x + 29;
+	this.position.y = enemyPos.y + 42;
+	this.speed = 5;
+	this.alive = true;
+	this.count = 0;
+
+	this.vector.x = this.position.x - myPos.x;
+	this.vector.y = this.position.y - myPos.y;
+	var length = Math.sqrt(this.vector.x * this.vector.x + this.vector.y * this.vector.y);
+	this.vector.x /= length;
+	this.vector.y /= length;
+
+
+}
+
+EnemyShot.prototype.move = function(){
+	this.count++;
+	this.position.x -= this.vector.x * this.speed;
+	this.position.y -= this.vector.y * this.speed;
+	if(this.position.x < 0 || this.position.x + 6 >360 || this.position.y < 0 || this.position.y + 6 > 360){
+		this.alive = false;
+	}	
+}
+
+
+function DisApp(){
 	this.position = new Point();
 	this.count = 0;
 	this.alive = false;
 }
 
-Bomb.prototype.set = function(p){
-	this.position.x = p.x + 15;
-	this.position.y = p.y;
+DisApp.prototype.set = function(p){
+	this.position.x = p.x - 10;
+	this.position.y = p.y - 21;
 	this.alive = true;
 	this.count = 0;
 }
--- main.js
+++ main.js
@@ -2,14 +2,19 @@
 var screen, info;
 var run = true;
 var fps = 1000 / 30;
-var mouse = new Point();
 var fire = false;
 var ctx;
 var counter = 0;
 var score = 0;
 
+var left = false;
+var up = false;
+var right = false;
+var down = false;
+
 var SHOT_MAX = 10;
 var ENEMY_MAX = 15;
+var ENEMY_SHOT_MAX = 100;
 var p = new Point();
 var i,j,k;
 // - main ---------------------------------
@@ -23,42 +28,56 @@ for(i = 0;i < ENEMY_MAX;i++){
 	enemy[i] = new Enemy();
 }
 
-var bomb = new Array(SHOT_MAX);
-for(i = 0;i < SHOT_MAX;i++){
-	bomb[i] = new Bomb();
+var enemyShot = new Array(ENEMY_SHOT_MAX)
+for(i = 0;i<ENEMY_SHOT_MAX;i++){
+	enemyShot[i] = new EnemyShot();
 }
+
+var disapp = new Array(SHOT_MAX);
+for(i = 0;i < SHOT_MAX;i++){
+	disapp[i] = new DisApp();
+}
+
+var chara = new Character();
+chara.position.y = 300;
+chara.position.x = 0;
 
 window.onload = function(){
 	
-	screen = document.getElementById('screen');
+	screen = document.getElementById("screen");
 	screen.width = 360;
 	screen.height = 360;
 	
 	ctx = screen.getContext('2d');
 	
-	screen.addEventListener('mousemove', mouseMove, true);
-	window.addEventListener('mousedown', mouseDown, true);
-	
-	info = document.getElementById('info');
+	window.addEventListener("keydown",keyDown,true);
+	window.addEventListener("keyup",keyUp,true);
+
+	info = document.getElementById("info");
 	
 	Asset.loadAssets();
-	
-	var chara = new Character();
-	chara.position.y = 300;
-	
+		
 	// レンダリング
 	(function(){
-		info.innerHTML = mouse.x + ' : ' + mouse.y + "<br>" + score;
+		info.innerHTML = "score:" + score;
 		counter++;
 		ctx.clearRect(0,0,screen.width,screen.height);
 		
-		chara.position.x = mouse.x - 45;
+		//移動
+		if(left)  chara.move("left");
+		if(up)    chara.move("up");
+		if(right) chara.move("right");
+		if(down)  chara.move("down");
 
+		//衝突
 		for(i = 0;i < ENEMY_MAX;i++){
 			if(enemy[i].alive){
+				if(CollisionEnemy(enemy[i].position,chara.position)){
+					run = false;
+				}
 				for(j = 0;j < SHOT_MAX;j++){
 					if(charaShot[j].alive){
-						if(Collision(enemy[i].position,charaShot[j].position)){
+						if(CollisionMyShot(enemy[i].position,charaShot[j].position)){
 							enemy[i].alive = false;
 							charaShot[j].alive = false;
 							switch(enemy[i].speed){
@@ -70,27 +89,42 @@ window.onload = function(){
 									break;
 							}
 							for(k = 0;k < SHOT_MAX;k++){
-								if(!bomb[k].alive){
-									bomb[k].set(enemy[i].position);
+								if(!disapp[k].alive){
+									disapp[k].set(enemy[i].position);
 								}
 							}
 						}
 					}
 				}
+
 			}
 		}
 
+		for(i = 0;i < ENEMY_SHOT_MAX;i++){
+			if(CollisionEnemyShot(enemyShot[i].position,chara.position)){
+				run = false;
+			}
+		}
+
+
+
+		//消滅画像の表示
 		for(i = 0;i < SHOT_MAX;i++){
-			if(bomb[i].alive){
-				ctx.drawImage(Asset.images["bomb"],bomb[i].position.x,bomb[i].position.y);
-				if(bomb[i].count <= 20){
-					bomb[i].alive = false;
+			if(disapp[i].alive){
+				disapp[i].count++;
+				if(disapp[i].count < 10){
+					ctx.drawImage(Asset.images["disapp1"],disapp[i].position.x,disapp[i].position.y);
 				}
-				bomb[i].count++;
+				else if(disapp[i].count < 20){
+					ctx.drawImage(Asset.images["disapp2"],disapp[i].position.x,disapp[i].position.y);
+				}
+				else{
+					disapp[i].alive = false;
+				}
 			}
 		}
 
-
+		//enemy出現
 		if(counter % 25 == 0){
 			for(i = 0;i < ENEMY_MAX;i++){
 				if(!enemy[i].alive){
@@ -110,6 +144,7 @@ window.onload = function(){
 
 		for(i = 0;i < ENEMY_MAX;i++){
 			if(enemy[i].alive){
+				//enemy描写
 				enemy[i].move();
 				if(enemy[i].speed == 5){
 					ctx.drawImage(Asset.images["kinoko"],enemy[i].position.x,enemy[i].position.y);	
@@ -117,10 +152,38 @@ window.onload = function(){
 				else{
 					ctx.drawImage(Asset.images["takenoko"],enemy[i].position.x,enemy[i].position.y);
 				}
+
+				//enemyShot出現
+				if(enemy[i].count % 10 == 0){
+					for(j = 0;j < ENEMY_SHOT_MAX;j++){
+						if(!enemyShot[j].alive){
+							enemyShot[j].set(enemy[i].position,chara.position);
+							break;
+						}
+					}
+				}
 			}
 		}
 
+		//enemyShot描写
+		for(i = 0;i < ENEMY_SHOT_MAX;i++){
+			if(enemyShot[i].alive){
+				enemyShot[i].move();
+				if(enemyShot[i].count < 8){
+					ctx.drawImage(Asset.images["enemyshot1"],enemyShot[i].position.x,enemyShot[i].position.y);
+				}
+				else if(enemyShot[i].count < 16){
+					ctx.drawImage(Asset.images["enemyshot2"],enemyShot[i].position.x,enemyShot[i].position.y);
+				}
+				else{
+					enemyShot[i].count = 0;
+					ctx.drawImage(Asset.images["enemyshot2"],enemyShot[i].position.x,enemyShot[i].position.y);
+				}
 
+			}
+		}
+
+		//shot出現
 		if(fire){
 			for(i = 0;i < SHOT_MAX;i++){
 				if(!charaShot[i].alive){
@@ -147,13 +210,48 @@ window.onload = function(){
 
 // - event ---------------------------------------
 function mouseMove(event){
-	mouse.x = event.clientX - screen.offsetLeft;
-	mouse.y = event.clientY - screen.offsetTop;
 }
 
-function mouseDown(event){
-	fire = true;
+function keyDown(event){
+	var ck = event.keyCode;
+	switch(ck){
+		case 37:
+			left = true;
+			break;
+		case 38:
+			up = true;
+			break;
+		case 39:
+			right = true;
+			break;
+		case 40:
+			down = true;
+			break;
+		case 32:
+			fire = true;
+			break;
+	}
 }
+
+function keyUp(event){
+	var ck = event.keyCode;
+	switch(ck){
+		case 37:
+			left = false;
+			break;
+		case 38:
+			up = false;
+			break;
+		case 39:
+			right = false;
+			break;
+		case 40:
+			down = false;
+			break;
+
+	}
+}
+
 
 // - asset -------------
 var Asset = {};
@@ -162,7 +260,10 @@ Asset.assets = [
 	{type:"image",name:"shot",src:"assets/shot.png"},
 	{type:"image",name:"kinoko",src:"assets/kinoko.png"},
 	{type:"image",name:"takenoko",src:"assets/takenoko.png"},
-	{type:"image",name:"bomb",src:"assets/bomb.png"}
+	{type:"image",name:"disapp1",src:"assets/disapp1.png"},
+	{type:"image",name:"disapp2",src:"assets/disapp2.png"},
+	{type:"image",name:"enemyshot1",src:"assets/enemyshot1.png"},
+	{type:"image",name:"enemyshot2",src:"assets/enemyshot2.png"}
 ];
 
 Asset.images = {};
@@ -195,13 +296,28 @@ Asset._loadImage = function(asset,onLoad){
 };
 
 //衝突判定
-function Collision(e_p,s_p){
-	if(e_p.x > (s_p.x +5)) return false;
-	if((e_p.x + 90) < (s_p.x +5)) return false;
-	if((e_p.y + 45) < s_p.y) return false;
+function CollisionMyShot(enemyPos,myShotPos){
+	if(enemyPos.x      > myShotPos.x + 1) return false;
+	if(enemyPos.x + 64 < myShotPos.x + 1) return false;
+	if(enemyPos.y + 42 < myShotPos.y    ) return false;
 	return true;
 }
 
+function CollisionEnemyShot(enemyShotPos,myPos){
+	if(enemyShotPos.x + 6 < myPos.x     )return false;
+	if(enemyShotPos.x     > myPos.x + 32)return false;
+	if(enemyShotPos.y + 6 < myPos.y     )return false;
+	if(enemyShotPos.y     > myPos.y + 48)return false;
+	return true;
 
+}
+
+function CollisionEnemy(enemyPos,myPos){
+	if(enemyPos.x      > myPos.x + 32)return false;
+	if(enemyPos.x + 64 < myPos.x     )return false;
+	if(enemyPos.y + 42 < myPos.y     )return false;
+	if(enemyPos.y      > myPos.y + 48)return false;
+	return true;
+}
 
 
